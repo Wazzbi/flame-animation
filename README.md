a952de9f9c47e2f485504b9a710f24391dba02fb
Wazzbi <davidnovotny47@gmail.com>
2020-02-18 11:10:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tgdf8f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tgdf8f)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  boxes = [1,2,3];
+  
+  // !!!TODO: OHEN VE VLASTNI KOMPONENTE
+
+}
